83e323081b35adca35eee1570bef96a959a62ec9
Samuel Oliveira <samuel_o.assis@hotmail.com>
2024-02-03 20:44:40 -0300

    Orientação a objetos
    Iniciando Orientação a Objetos.

--- Stage 06/Conceitos de SPA/src/index.js
+++ Stage 06/Conceitos de SPA/src/index.js
@@ -1,3 +1,4 @@
+/*
 const routes = {
   '/': '/pages/home.html',
   '/about': '/pages/about.html',
@@ -5,27 +6,10 @@ const routes = {
   404: '/pages/404.html',
 }
 
-function route (event) {
-  event = event || window.event
-  event.preventDefault()
-
-  window.history.pushState({}, "", event.target.href)
-
-  handle()
-}
-
-function handle() {
-  const { pathname } = window.location
-  const route = routes[pathname] || routes[404]
-
-  fetch(route)
-  .then(data => data.text())
-  .then(html => {
-    document.querySelector('#app').innerHTML = html
-  })
-}
 
 handle()
 window.onpopstate = () => handle()
 
 window.route = () => route()
+
+*/
